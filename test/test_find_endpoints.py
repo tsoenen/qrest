a248e8f0c5b0a24ff88eac1f20530d74506d8a5e
Pieter Swinkels <swinkepi@by0q1y.eu.seeds.basf.net>
2020-07-10 09:22:23 +0200

    VSDEV-1762: Add code to dynamically find APIConfig class
    The code is there, but isn't hooked up yet. In its current state, it's more of a
    proof of concept.

--- test/test_find_endpoints.py
+++ test/test_find_endpoints.py
@@ -20,6 +20,25 @@ class SecondResourceConfig(ResourceConfig):
     method = "GET"
 
 
+def find_api_config_class(module):
+    for name, value in inspect.getmembers(module):
+        # the member should be defined in the given module
+        if inspect.getmodule(value) == module:
+            # issubclass requires its first argument to be a class
+            if inspect.isclass(value) and issubclass(value, APIConfig):
+                return value
+
+
+class FindAPIConfigTests(unittest.TestCase):
+    def test_return_APIConfig_from_given_module(self):
+        current_module = inspect.getmodule(FindEndpointsConfig)
+        config_class = find_api_config_class(current_module)
+        self.assertTrue(
+            issubclass(config_class, FindEndpointsConfig),
+            f"Class {config_class} should be a subclass of FindEndpointsConfig",
+        )
+
+
 def find_endpoints(config_class):
     endpoints = {}
     module = inspect.getmodule(config_class)
